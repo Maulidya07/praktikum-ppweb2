a9f334302c1e48906cd64ee75afc4ddd964bd124
Maulidya Syifa Aszahra <maulidyasyifaazzahra@gmail.com>
2025-02-28 20:06:12 +0700

    Praktikum Pertemuan 2

---
+++ Pertemuan2/variable_user.php
@@ -0,0 +1,10 @@
+<?php
+    //Mendifinisikan variables
+    $nama = "Mauri";
+    $umur = 18;
+    $bb = 38;
+
+    echo "Nama saya adalah : ".$nama;
+    echo "<br>Umur : ".$umur. " Tahun";
+    echo "<br>Berat badan : ".$bb. " Kg";
+?>
